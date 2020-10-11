92e9b70f63605e81d2945b70b2b34208660aebcc
plocket <52798256+plocket@users.noreply.github.com>
2020-10-11 09:29:56 -0400

    Try to make _their_ world work

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -2,6 +2,8 @@ const { setWorldConstructor } = require('cucumber');
 const puppeteer = require('puppeteer');
 const scope = require('./scope');
 
+console.trace('world');
+
 const World = function() {
   scope.driver = puppeteer;
   scope.context = {};
