bd75c769fdf2609e5a456af183cd7ffe7fa7a730
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-10-13 08:46:54 -0400

    Start wizard to set up an Assembly Line interview with this

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.IntegratedTesting',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='Docassembly Line Integrated Testing: Experiments',
+      long_description='# Docassembly Line Integrated Testing: Experiments\r\n\r\n## Uses\r\n- mocha\r\n- chai\r\n- puppeteer\r\n- cucumber\r\n- docassemble\r\n\r\nNote: This is not behavior driven development. This is unit testing. End-to-end unit testing is not the typical use of libraries like cucumber, but it is what these projects need to ease the development process.\r\n',
       long_description_content_type='text/markdown',
       author='',
       author_email='52798256+plocket@users.noreply.github.com',
